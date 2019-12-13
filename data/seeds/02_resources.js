ce847aa2048f57740b78269455f4afafa8b7fce7
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-13 09:38:57 -0800

    seeds built and "complete"

--- data/seeds/02_resources.js
+++ data/seeds/02_resources.js
@@ -1,13 +1,27 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('resources').del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('resources').insert([
+
+                {
+                    resource_name: "Hamper",
+                    resource_description: "Something to carry the clothes."
+                },
+                {
+                    resource_name: "Tide Pod",
+                    resource_description: "Something to wash the clothes."
+                },
+                {
+                    resource_name: "Dryer Sheet",
+                    resource_description: "Something to give the clothes a nice scent."
+                },
+                {
+                    resource_name: "Hangers",
+                    resource_description: "Something to hang the clothes."
+                },
+            ]);
+        });
 };
--- data/seeds/03_tasks.js
+++ data/seeds/03_tasks.js
@@ -1,13 +1,34 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('tasks').del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('tasks').insert([
+                {
+                    task_description: "At some point, the clothes must be consolidated.",
+                    task_note: "Use the Force",
+                    completed: false,
+                    project_id: 1
+                },
+                {
+                    task_description: "Whites and Colors? Myths! WASH IT ALL!",
+                    task_note: "But keep those towels seperate.",
+                    completed: false,
+                    project_id: 1
+                },
+                {
+                    task_description: "We hang dry out shirts and jackets... machine dry everything else!",
+                    task_note: "Don't let that SHIrT shrink!",
+                    completed: false,
+                    project_id: 1
+                },
+                {
+                    task_description: "Now, fold and sort. Classic folds only nothing fancy.",
+                    task_note: "Channel mom here, it helps.",
+                    completed: false,
+                    project_id: 1
+                },
+            ]);
+        });
 };
