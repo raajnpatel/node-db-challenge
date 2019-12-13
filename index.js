df096424dfec1b209c59fc3e194fec53220b5403
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-13 10:54:03 -0800

    MVP complete - API working properly!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 4444;
+const port = process.env.PORT || 4444;
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+server.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
 });
